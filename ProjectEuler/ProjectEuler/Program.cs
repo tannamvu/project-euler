0e6a1a319d7543167f4c0b18261bfb2f63d20836
Nam Vu <tannamvu@gmail.com>
2013-09-17 14:12:40 +0100

    initial project push

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1;
+            var y = 2;
+            var sum = 2;
+
+            while ((x + y) < 4000000)
+            {
+                var total = x + y;
+                Console.WriteLine(total);
+                x = y;
+                y = total;
+
+                if (total % 2 == 0)
+                {
+                    sum += total;
+                }
+            }
+
+            Console.WriteLine("Sum of even values is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
